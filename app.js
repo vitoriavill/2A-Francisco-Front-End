21cbb06b542a7c87fb17c13c41a97b15b8b0d247
vitoriavill <138012278+vitoriavill@users.noreply.github.com>
2023-09-21 09:53:03 -0300

    continuacao

--- app.js
+++ app.js
@@ -1,2 +1,5 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("aplicação está se conectando ao banco de dados")
++// mais um console
++ console.log("O paulo fez esse comit sem voce ver a aula!")
++
